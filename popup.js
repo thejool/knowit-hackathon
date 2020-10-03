b534518a41ffcd2e552f89fcfb4ccc0fbe7a6919
Joel Gustafsson <joel.gustafsson@knowit.se>
2020-10-03 10:49:56 +0200

    Changed to one person log

--- popup.js
+++ popup.js
@@ -1,15 +1,46 @@
-let changeColor = document.getElementById('changeColor');
+let startCaptureScreen = document.getElementById('startCaptureScreen');
+let stopCaptureScreen = document.getElementById('stopCaptureScreen');
 
-chrome.storage.sync.get('color', function(data) {
-	changeColor.style.backgroundColor = data.color;
-	changeColor.setAttribute('value', data.color);
-});
+// chrome.storage.sync.get('color', function(data) {
+// 	changeColor.style.backgroundColor = data.color;
+// 	changeColor.setAttribute('value', data.color);
+// });
 
-changeColor.onclick = function(element) {
-	let color = element.target.value;
-	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		chrome.tabs.executeScript(
-			tabs[0].id,
-			{ code: 'document.body.style.backgroundColor = "' + color + '";' });
-	});
+const startCapture = async (displayMediaOptions) => {
+	console.log('startCapture')
+	let captureStream = null;
+
+	try {
+		captureStream = await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
+		console.log('captureStream')
+		console.log(captureStream)
+	} catch(err) {
+		console.error("Error: " + err);
+	}
+	return captureStream;
+}
+
+startCaptureScreen.onclick = async () => {
+	console.log('startCaptureScreen')
+	const result = await startCapture()
+	console.log('result')
+	console.log(result)
+
+	// chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+	// 	chrome.tabs.executeScript(
+	// 		tabs[0].id,
+	// 		{ code: 'document.body.style.backgroundColor = "' + color + '";' });
+	// });
+};
+
+stopCaptureScreen.onclick = async () => {
+	const result = await stopCapture()
+	console.log('result')
+	console.log(result)
+
+	// chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+	// 	chrome.tabs.executeScript(
+	// 		tabs[0].id,
+	// 		{ code: 'document.body.style.backgroundColor = "' + color + '";' });
+	// });
 };
