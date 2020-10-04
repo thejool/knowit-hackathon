8e85717b7bb46dc5bbf648286626259afd44dec9
Henrik Viksten <henrik.viksten@knowit.se>
2020-10-04 09:52:21 +0200

    det funkar

---
+++ ui/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Timeline from "./Timeline";
+
+function App() {
+  return (
+    <div className='App'>
+      <Timeline />
+    </div>
+  );
+}
+
+export default App;
